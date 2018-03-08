20047dbbcaf6ebc5ad853daf999a776ff4e17cda
cooper-mj <coopermj@stanford.edu>
2018-03-07 23:13:40 -0800

    Reorganized, added 'quine' program

--- C++/temp.cpp
+++
@@ -1,7 +0,0 @@
-/* 
- * File: temp.cpp
- * Author: Michael Cooper
- * 
- * Description: This is a placeholder for C++ files in this folder.
- * 
- */
---
+++ quine.py
@@ -0,0 +1,52 @@
+'''
+A quine is a nonempty computer program which takes no input and outputs a
+copy of its own source code. I decided to have some fun writing a quine.
+This program is a quine; when run, it takes no inputs, and produces a copy
+of its source code.
+'''
+
+code_source = [
+"'''",
+"A quine is a nonempty computer program which takes no input and outputs a",
+"copy of its own source code. I decided to have some fun writing a quine.",
+"This program is a quine; when run, it takes no inputs, and produces a copy",
+"of its source code.",
+"'''",
+"",
+"##",
+"",
+"def printSource():",
+"\tfor line in code_source:",
+'\t\tif line == "##":',
+'\t\t\tprint("code_source = [")',
+"\t\t\tfor line_2 in code_source:",
+"\t\t\t\tprint(repr(line_2))",
+'\t\t\tprint("]")',
+"\t\tprint(line)",
+"",
+"",
+"def main():",
+"\tprintSource()",
+"",
+"",
+'if __name__ == "__main__":',
+"\tmain()"
+]
+    
+def printSource():
+    for line in code_source:
+        if line == "##":
+            print("code_source = [")
+            for line_2 in code_source:
+                print(repr(line_2))
+            print("]")
+        else:
+            print(line)
+    
+
+def main():
+    printSource()
+
+
+if __name__ == "__main__":
+    main()
